8bb5dd974518302a82aafe07c4ea765053dd436a
Ajoy563 <dasajoy661@gmail.com>
2025-08-28 21:59:20 +0530

    Update Intersection349.java

--- Intersection349.java
+++ Intersection349.java
@@ -1,5 +1,6 @@
-class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
+import java.util.*;
+class Intersection349 {
+    public static int[] intersection(int[] nums1, int[] nums2) {
         HashSet<Integer> set = new HashSet<>();
         for(Integer i : nums1) {
             set.add(i);
@@ -17,5 +18,12 @@ class Solution {
             resultset[i++] = num;
         }
         return resultset; 
+    }
+    public static void main(String[] args) {
+        int nums1[] = {7,3,9};
+        int nums2[] = {6,3,9,2,9,4};
+        int result[] = intersection(nums1, nums2);
+        System.out.println(Arrays.toString(result));
+
     }
 }
